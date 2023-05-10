1e4634c6f9946e34e96a3a506a2747884013a646
shpark0913 <shpark097754@gmail.com>
2023-05-11 08:37:00 +0900

    chap5_BFS 개념 정리

--- chap5_DFS,BFS/DFS_1388.py
+++ chap5_DFS,BFS/DFS_1388.py
@@ -1,6 +1,7 @@
 # 백준 1388 (실버 4)
 # https://www.acmicpc.net/problem/1388
-# Python3 / 메모리 : KB / 시간 : ms
+# Python3 / 메모리 : 31256KB / 시간 : 52ms
+# input = sys.stdin.readline 하면 시간 : 44ms
 
 # 바닥의 세로 크기 : N, 가로 크기 : M
 N, M = map(int, input().split())
@@ -8,11 +9,34 @@ N, M = map(int, input().split())
 floor = [list(input()) for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
 
-def dfs(floor, i, j, visited):
+# 필요한 나무 판자의 개수
+boardNum = 0
+
+def dfs(floor, visited, i, j):
+    global boardNum
+    # 방문 처리
     visited[i][j] = True
+    boardNum += 1
+
     if floor[i][j] == '-':
-        if floor[i+1][j] and floor[i+1][j] == '-':
-            dfs(floor, i + 1, j, visited)
+        while 1:
+            if j + 1 < M and floor[i][j+1] == '-':
+                visited[i][j+1] = True
+                j += 1
+            else:
+                break
     elif floor[i][j] == '|':
-        if floor[i][j+1] and floor[i][j+1] == '|':
-            dfs(floor, i, j + 1, visited)
+        while 1:
+            if i + 1 < N and floor[i+1][j] == '|':
+                visited[i+1][j] = True
+                i += 1
+            else:
+                break
+
+
+for i in range(N):
+    for j in range(M):
+        if visited[i][j] == False:
+            dfs(floor, visited, i, j)
+
+print(boardNum)
