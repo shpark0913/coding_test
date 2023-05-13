5385c050739ca5b11581c9e65259ed4f2a0c3105
shpark0913 <shpark097754@gmail.com>
2023-05-13 14:40:47 +0900

    DFS, BFS 문제 해결 완료 (각각 정의해서 해결)

--- chap5_DFS,BFS/DFS, BFS_1260.py
+++ chap5_DFS,BFS/DFS, BFS_1260.py
@@ -1,3 +1,8 @@
+# 백준 1260 (실버 2)
+# https://www.acmicpc.net/problem/1260
+# Python3 / 메모리 : 34176KB / 시간 : 468ms
+# PyPy3 / 메모리 : 117424KB / 시간 : 196ms
+
 from collections import deque
 
 # 정점의 개수, 간선의 개수, 탐색 시작 정점 번호
@@ -6,23 +11,43 @@ N, M, V = map(int, input().split())
 visited_DFS = [False] * (N + 1)
 visited_BFS = [False] * (N + 1)
 
+result_DFS = []
+result_BFS = []
+
 graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     s, e = map(int, input().split())
     graph[s].append(e)
-    if s not in graph[e]:
-        graph[e].append(s)
+    graph[e].append(s)
 
 for elt in graph:
-    elt = sorted(elt)
+    elt.sort()
 
 def dfs(graph, visited, V):
+    global result_DFS
     visited[V] = True
-    print(V, end=' ')
+    result_DFS.append(V)
     for i in graph[V]:
         if not visited[i]:
             dfs(graph, visited, i)
 
-# def bfs(graph, visited, V):
+
+def bfs(graph, visited, V):
+    global result_BFS
+    Queue = deque([V])
+    visited[V] = True
+    while Queue:
+        q = Queue.popleft()
+        result_BFS.append(q)
+        for elt in graph[q]:
+            if not visited[elt]:
+                visited[elt] = True
+                Queue.append(elt)
+
+
 dfs(graph, visited_DFS, V)
+bfs(graph, visited_BFS, V)
+
+print(*result_DFS)
+print(*result_BFS)
