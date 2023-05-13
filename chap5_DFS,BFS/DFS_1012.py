290dccafea09a78fff5b198e7bfa8c95d3249d17
shpark0913 <shpark097754@gmail.com>
2023-05-13 15:34:05 +0900

    :recycle: 코드 리팩토링
    - 코드 리팩토링을 통해 간략한 코드 작성

--- chap5_DFS,BFS/DFS_1012.py
+++ chap5_DFS,BFS/DFS_1012.py
@@ -8,16 +8,14 @@ sys.setrecursionlimit(10**7)
 T = int(input())  # 테스트 케이스의 수
 
 
-def dfs(graph, isvisited, a, b):
-    isvisited[a][b] = True
+def dfs(graph, a, b):
+    graph[a][b] = 0
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     for k in range(4):
-        nx = a + dx[k]
-        ny = b + dy[k]
-        if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]):
-            if (not isvisited[nx][ny]) and graph[nx][ny] == 1:
-                dfs(graph, isvisited, nx, ny)
+        nx, ny = a + dx[k], b + dy[k]
+        if 0 <= nx < len(graph) and 0 <= ny < len(graph[0]) and graph[nx][ny]:
+            dfs(graph, nx, ny)
 
 
 for t in range(T):
@@ -28,18 +26,14 @@ for t in range(T):
 
     # 배추밭 (1: 배추, 0: 빈 위치)
     field = [[0] * N for _ in range(M)]
-
-    # 방문 여부
-    visited = [[False] * N for _ in range(M)]
-
     for _ in range(K):
         X, Y = map(int, input().split())  # 배추의 위치
         field[X][Y] = 1
 
     for i in range(len(field)):
         for j in range(len(field[0])):
-            if (not visited[i][j]) and field[i][j] == 1:
-                dfs(field, visited, i, j)
+            if field[i][j]:
+                dfs(field, i, j)
                 bugNum += 1
 
     print(bugNum)
