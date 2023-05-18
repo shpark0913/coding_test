8099bf7f0b5fc2e931b275ec2a5a006cf8f20026
shpark0913 <shpark097754@gmail.com>
2023-05-18 23:09:30 +0900

    chap5_BFS(백준 2644)를 DFS 로 해결

--- chap5_DFS,BFS/BFS_2644.py
+++ chap5_DFS,BFS/BFS_2644.py
@@ -1,53 +1,35 @@
 # 백준 2644 (실버 2)
 # https://www.acmicpc.net/problem/2644
-# Python3 / 메모리 : KB / 시간 : ms
-# PyPy3 / 메모리 : KB / 시간 : ms
+# Python3 / 메모리 : 31256KB / 시간 : 40ms
+# PyPy3 / 메모리 초과
 from collections import deque
 import sys
-
+sys.setrecursionlimit(3000000)
 input = sys.stdin.readline
 
 # 사람의 수
 n = int(input())
 
-# 촌수를 계산해야 하는 두 사람의 번호
 a, b = map(int, input().split())
 
-# 부모 자식 관계의 개수
-m = int(input())
-
-# 부모, 자식 관계를 담는 리스트
 relations = [[] for _ in range(101)]
-
-# 방문 기록
-visited = [False for _ in range(101)]
-
-# 촌수 기록하는 object
-relations_object = {}
+visited = [0 for _ in range(101)]
+# 부모, 자식 관계의 수
+m = int(input())
 
 for _ in range(m):
     x, y = map(int, input().split())
     relations[x].append(y)
     relations[y].append(x)
 
-def bfs(V, S):
-    queue = deque([V])
-    relation = 1
-    while queue:
-        q = queue.popleft()
-        visited[q] = True
-        for i in relations[q]:
-            if not visited[i]:
-                queue.append(i)
-                if i in relations_object.keys():
-                    relations_object[i].append(relation)
-                else:
-                    relations_object[i] = [relation]
-        relation += 1
+cnt = 0
+def dfs(V):
+    for i in relations[V]:
+        if visited[i] == 0:
+           visited[i] = visited[V] + 1
+           dfs(i)
 
-bfs(a, b)
-print(relations_object)
-if b in relations_object.keys():
-    print(min(relations_object[b]))
-else:
-    print(-1)
+dfs(a)
+
+print(visited[b] if visited[b] else -1)
+
