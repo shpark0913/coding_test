b1c781dc1f2054f29950b290ca37eee088416b36
shpark0913 <shpark097754@gmail.com>
2023-05-19 00:01:02 +0900

    chap5_백준 2644 BFS, DFS로 각각 풀이 완료

--- chap5_DFS,BFS/BFS_2644.py
+++ chap5_DFS,BFS/BFS_2644.py
@@ -1,8 +1,48 @@
 # 백준 2644 (실버 2)
 # https://www.acmicpc.net/problem/2644
+# Python3 / 메모리 : 34120KB / 시간 : 64ms
+# PyPy3 / 메모리 : 114436KB / 시간 : 136ms
+# BFS 풀이
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+# 사람의 수
+n = int(input())
+
+a, b = map(int, input().split())
+
+relations = [[] for _ in range(101)]
+visited = [0 for _ in range(101)]
+
+# 부모, 자식 관계의 수
+m = int(input())
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    relations[x].append(y)
+    relations[y].append(x)
+
+def bfs(V):
+    queue = deque([V])
+    while queue:
+        q = queue.popleft()
+        for i in relations[q]:
+            if not visited[i]:
+                visited[i] = visited[q] + 1
+                queue.append(i)
+
+bfs(a)
+
+print(visited[b] if visited[b] else -1)
+
+
+###################################################
 # Python3 / 메모리 : 31256KB / 시간 : 40ms
 # PyPy3 / 메모리 초과
-from collections import deque
+# DFS 풀이
 import sys
 sys.setrecursionlimit(3000000)
 input = sys.stdin.readline
@@ -22,7 +62,6 @@ for _ in range(m):
     relations[x].append(y)
     relations[y].append(x)
 
-cnt = 0
 def dfs(V):
     for i in relations[V]:
         if visited[i] == 0:
