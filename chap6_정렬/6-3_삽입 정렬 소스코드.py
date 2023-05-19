e395726c98e26924f95755408ff615ce361614f8
shpark0913 <shpark097754@gmail.com>
2023-05-19 17:38:45 +0900

    chap6_코드 주석 오타 수정

--- chap6_정렬/6-3_삽입 정렬 소스코드.py
+++ chap6_정렬/6-3_삽입 정렬 소스코드.py
@@ -6,7 +6,7 @@ for i in range(1, len(array)):
         # 한 칸씩 왼쪽으로 이동
         if array[j] < array[j - 1]:
             array[j], array[j - 1] = array[j - 1], array[j]
-        # 자신보다 작은 데이터를 만나면 멈ㅊㅁ
+        # 자신보다 작은 데이터를 만나면 멈춤
         else:
             break
 
