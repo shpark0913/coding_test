f1b19167159c6504102e59f987d6d565a5382bcf
shpark0913 <shpark097754@gmail.com>
2023-05-21 19:36:45 +0900

    chap7_이진탐색 학습

---
+++ chap7_이진 탐색/7-1_순차 탐색 소스코드.py
@@ -0,0 +1,7 @@
+# 순차 탐색 소스코드 구현
+def sequential_search(n, target, array):
+    # 각 원소를 하나씩 확인하며
+    for i in range(n):
+        # 현재의 원소가 찾고자 하는 원소와 동일한 경우
+        if array[i] == target:
+            return i + 1    # 현재의 위치 반환(인덱스는 0부터 시작이므로 1 더하기)
